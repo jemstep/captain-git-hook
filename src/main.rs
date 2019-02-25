c47e54a743bcdacc6846da31f7137311c662ff36
Justin Worthe <justin@jemstep.com>
2019-02-25 13:56:51 +0200

    Shortened CLI command name to be easier to use interactively

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod policies;
 use crate::policies::*;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "captain-git-hook", about = "An example of StructOpt usage.")]
+#[structopt(name = "Captain Git Hook", about = "A collection of tools for more opinionated Git usage")]
 enum Opt {
     /// Git hook called before opening an editor with a commit message
     #[structopt(name = "prepare-commit-msg")]
@@ -61,7 +61,7 @@ fn install_hooks() -> Result<(), Box<Error>> {
     prepare_commit_msg.set_permissions(PermissionsExt::from_mode(0o750))?;
 
     writeln!(prepare_commit_msg, "#!/bin/sh")?;
-    writeln!(prepare_commit_msg, "captain-git-hook prepare-commit-msg \"$@\"")?;
+    writeln!(prepare_commit_msg, "capn prepare-commit-msg \"$@\"")?;
 
     Ok(())
 }
