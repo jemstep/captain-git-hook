8d90620333d6c981f323964f903ee06577c8d440
Justin Worthe <justin@jemstep.com>
2019-06-04 11:39:35 +0200

    First end to end acceptance test run for verifying signing of commits

--- tests/policies_test.rs
+++ tests/policies_test.rs
@@ -4,19 +4,20 @@ use capn::git::LiveGit;
 use capn::gpg::LiveGpg;
 
 #[test]
-#[ignore("This functionality has not been implemented yet")]
 fn verify_git_commits() {
+    std::env::set_current_dir("./tests/test-repo.git").unwrap();
+    
     let config = VerifyGitCommitsConfig {
         author_domain : "jemstep.com".to_string(), 
         committer_domain : "jemstep.com".to_string(),
-        keyserver : "KEYSERVER".to_string(),
-        team_fingerprints_file: "gpg/TEAM_FINGERPRINTS".to_string(),
+        keyserver : "hkp://pgp.jemstep.com:80".to_string(),
+        team_fingerprints_file: "TEAM_FINGERPRINTS".to_string(),
         recv_keys_par : true,
         verify_email_addresses: true,
         verify_commit_signatures: true,
         verify_different_authors: true
     };
 
-    let result = policies::verify_git_commits::<LiveGit, LiveGpg>(&config, "old_value","new_value","ref_name");
-    assert!(result.is_ok());
+    let result = policies::verify_git_commits::<LiveGit, LiveGpg>(&config, "0000000000000000000000000000000000000000","69841d34d7dbef6d70ea9f59419c9fed7749575f","master");
+    assert!(result.is_ok(), "Error: {:?}", result);
 }
